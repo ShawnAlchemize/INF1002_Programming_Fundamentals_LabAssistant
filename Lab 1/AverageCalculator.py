93876ee4957600bf4875f22f9300e0b0ccff732e
ShawnAlchemize <yeo_jiekai@yahoo.com.sg>
2022-09-16 02:46:34 +0800

    updated lab 1

--- Lab 1/AverageCalculator.py
+++ Lab 1/AverageCalculator.py
@@ -1,7 +1,18 @@
-#### Average Calculator Program ###
+import sys
 
-"""
-Don't loser, try harder. Lmao.
-"""
+n = len(sys.argv)   # n = length of the list 
+                    # items in the list 'n' (./easy.py , 3.14, 5, 7)
+n-=1                # n = n - 1
 
+# nameofpythonfile = sys.argv[0]
 
+a = float(sys.argv[1])      #int is 1, 2, 3, 4... 
+b = float(sys.argv[2])      #float is 1.0, 1.1, 2.1,2.222222, 3.0 etc...
+c = float(sys.argv[3])
+
+add = 0.0           # initialize 'add' 
+add = a + b + c     # add them together and store results in 'add'
+average = add/n     # divide the sum of numbers with the number of numbers 
+                    # / vs // ?
+
+print ("Average:", str('%.2f'%((round(average,2)))))    # fomatting the output
--- Lab 1/BMI_Calculator.py
+++ Lab 1/BMI_Calculator.py
@@ -1,5 +1,29 @@
-### BMI_Calculator.py
+import sys
+
+### Get the arguments from the command-line except the filename
+nameofpythonfile = sys.argv[0]  
+units = float(sys.argv[1])      
+a = float(sys.argv[2])      
+b = float(sys.argv[3])
 
 """
-Don't loser, try harder. Lmao.
+Check if units = imperial or metric. (if...else...)
+
+Calculate BMI with the corresponding units.
+BMI = weight(kg)/height2(m2) (Metric Units)
+BMI = 703·weight(lb)/height2(in2) (U.S. Units)
+
+Check BMI category.
 """
+
+### A function to check the BMI category
+def BMI_Category(BMI):
+        if (BMI <= 15):
+            return '\tSevere Thinness'
+        elif (16<= BMI <= 17):
+            return '\tModerate Thinness'
+        ### ...
+        ### ...
+        ### ...
+        elif (BMI > 40):
+            return '\tObese Class III'  
